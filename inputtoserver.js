24d688de1d706282d151b19fdaac1cebefe10191
untungsubagyo <untungsubagyo@gmail.com>
2018-03-16 11:36:02 +0700

    Initial commit

---
+++ inputtoserver.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    var table = $('#example').DataTable();
+ 
+    $('button').click( function() {
+        var data = table.$('input, select').serialize();
+        alert(
+            "The following data would have been submitted to the server: \n\n"+
+            data.substr( 0, 120 )+'...'
+        );
+        return false;
+    } );
+} );
